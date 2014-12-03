4ac5c7ceee3254de88ed6f4b4bfc6d0ea5b8aacd
RockyChronister <rocky@hyperiondesigned.com>
2014-12-02 18:17:44 -0600

    Signature

---
+++ wp-content/plugins/urban-donations-manager/class-urban-donations-manager.php
@@ -0,0 +1,10 @@
+<?php
+/*
+  Plugin Name: UrbanMilwaukee Political Donations Manager
+  Plugin URI:
+  Description: Core data management using Custom Post Types for the management of content in regard to political donations on http://UrbanMilwaukee.com
+  Author: Rocky Chronister
+  Version: 0.1.0
+  Author URI: http://www.hyperiondesigned.com/
+ */
+?>
